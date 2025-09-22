d46c6537dbaf8c6eb638b5c68e10e31ab015f706
Yehor <omelianenkoegor@gmail.com>
2025-09-22 17:51:23 +0200

    Big big updat
    e

--- player.gd
+++ player.gd
@@ -1,13 +1,19 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -700.0
+var dJump = true
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	elif is_on_floor():
+		dJump = true
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y += JUMP_VELOCITY
+	elif Input.is_action_just_pressed("jump") and not is_on_floor() and dJump == true:
+		dJump = false
+		velocity.y += JUMP_VELOCITY/2.5
 	var direction := Input.get_axis("left", "right")
 	
 	if direction:
